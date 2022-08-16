3ab0b0723b7ba690942dfeb50c8813f4cf989834
DilshanHarendra <harendracripto@gmail.com>
2022-08-16 11:33:00 +0530

    modify menu routes

--- routers/menuRouter.js
+++ routers/menuRouter.js
@@ -1,8 +1,27 @@
 const express = require('express')
+const Menus = require("../models/menus");
+const data = require("../data/menuItems.json");
 const router = express.Router()
 
 
+router.post('/', (req,res)=>{
 
+    try {
+        Menus.bulkCreate(data)
+        res.status(200).send(data)
+    }catch (err) {
+        res.status(400).send({message:err})
+    }
+})
+router.get('/',async (req,res)=>{
+
+    try {
+        let menus= await  Menus.findAll()
+        res.status(200).send(menus)
+    }catch (err) {
+        res.status(400).send({message:err})
+    }
+})
 
 
 
